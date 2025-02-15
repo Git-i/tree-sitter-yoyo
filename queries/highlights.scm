6f82753c55ee1d3db85291356c59a0258ad8896c
Eric Nwogbo <ericis2007@gmail.com>
2025-02-15 09:05:26 +0100

    Updates to syntax highlighting queries

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -10,10 +10,6 @@
     "for"
 ] @keyword.type
 [
-    "true"
-    "false"
-] @boolean
-[
     "if"
     "else"
 ] @keyword.conditional
@@ -26,32 +22,27 @@
     "const"
     "mut"
 ]@keyword.modifier
+"this"@variable.builtin
 (null_literal) @constant
 (line_comment) @comment
 (scope_expr (name_expr)@type)
+
+(bool_literal)@boolean
 (expression (name_expr)@variable)
 ((identifier) @variable.builtin (#eq? @variable.builtin "this"))
+(binary_expr (_) "." (expression (name_expr))@variable.member)
 
 (call_expr (expression (name_expr) @function.call))
-(call_expr .(expression (scope_expr [
-    (name_expr) @function.call
-    (generic_name_expr) @function.call
-].)))
 (call_expr (expression (binary_expr (expression (name_expr)@function.call). )))
-(call_expr (expression (binary_expr (expression (scope_expr [
-    (name_expr) @function.call
-    (generic_name_expr) @function.call
-].)). )))
+
 (type (identifier)@type)
 (integer_literal)@number
 (primitivetype)@type.builtin
 (var_decl (identifier)@variable)
 (function_decl .(identifier)@function)
-(class_struct_decl (identifier)@variable.member)
-(class_struct_decl .(identifier)@type)
+(class_struct_decl .(identifier)@type (identifier)@variable.member)
 (obj_literal .(name_expr)@type)
 (obj_literal (identifier)@variable.member)
 (string)@string
-(enum_decl (identifier)@constant)
-(enum_decl .(identifier)@type)
-(class_struct_decl)
+(enum_decl .(identifier)@type (identifier)@constant)
+(cond_extract_stat (identifier)@variable)
